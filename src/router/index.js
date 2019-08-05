aeeb5e7d30a1965b6f89e44ec9951dee55e69d95
Jorge Chiquín Valderrama <jorgechiquinv@gmail.com>
2019-08-05 14:41:49 -0400

    Router mode dynamic

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,7 @@ Vue.use(Router);
 
 export default new Router({
   routes,
-  //mode: "hash",
-  mode: "history",
+  mode: !process.env.CORDOVA_PLATFORM ? "history" : "hash",
   scrollBehviour(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
