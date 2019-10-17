c295c18d6e7b09ab465d3fc03cebe49e6bcf8b7b
Jorge Chiquín Valderrama <jorgechiquinv@gmail.com>
2019-10-17 16:19:01 -0400

    Change layout for refill

--- src/views/Pages/Refill/RefillStepper.vue
+++ src/views/Pages/Refill/RefillStepper.vue
@@ -2,21 +2,29 @@
   <v-stepper v-model="stage" class="h-full">
     <v-stepper-header>
       <v-stepper-step step="1" :complete="stage > 1"
-        >Datos de la recarga</v-stepper-step
+        >Seleccione una tarjeta</v-stepper-step
       >
       <v-divider></v-divider>
-      <v-stepper-step step="2" :complete="stage > 2"
+      <v-stepper-step step="2" :complete="stage > 2">Monto</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="3" :complete="stage > 3"
         >Clave de operaciones</v-stepper-step
       >
     </v-stepper-header>
 
     <v-stepper-content step="1" class="no-mrpd h-full">
-      <refill-form @next="goToOperationKey"></refill-form>
+      <select-instrument @next="goToAmount"></select-instrument>
     </v-stepper-content>
     <v-stepper-content step="2" class="no-mrpd h-full">
+      <refill-amount
+        :paymentInstrumentSelected="refillData.paymentInstrumentSelected"
+        @next="goToOperationKey"
+        @back="stage--"
+      ></refill-amount>
+    </v-stepper-content>
+    <v-stepper-content step="3" class="no-mrpd h-full">
       <operation-key
         @success="submitAll"
-        @next="stage++"
         @back="stage--"
         :operation_key_md5="user.operation_key"
       ></operation-key>
@@ -25,20 +33,22 @@
 </template>
 <script>
 import OperationKey from "../Pay/OperationKey";
-import RefillForm from "./RefillForm";
+import RefillAmount from "./RefillAmount";
+import SelectInstrument from "./SelectInstrument";
 import { mapGetters } from "vuex";
 import { postRefillApi } from "@/api/modules";
 
 export default {
   components: {
-    RefillForm,
-    OperationKey
+    RefillAmount,
+    OperationKey,
+    SelectInstrument
   },
   data() {
     return {
       stage: 1,
       refillData: {
-        payment_instrument_id: null,
+        paymentInstrumentSelected: null,
         amount: null
       }
     };
@@ -48,11 +58,19 @@ export default {
   },
   methods: {
     goToOperationKey(data) {
+      this.refillData.amount = data.amount;
+      this.stage = 3;
+    },
+    goToAmount(data) {
       this.refillData = { ...data };
       this.stage = 2;
     },
     async submitAll() {
-      var serviceResponse = await postRefillApi(this.refillData);
+      const body = {
+        amount: this.refillData.amount,
+        payment_instrument_id: this.refillData.paymentInstrumentSelected.id
+      };
+      var serviceResponse = await postRefillApi(body);
       const params = { text: serviceResponse.message.text };
       if (serviceResponse.ok) {
         window.getApp.$emit("SHOW_MESSAGE", params);
--- src/views/Pages/Refill/SelectInstrument.vue
+++ src/views/Pages/Refill/SelectInstrument.vue
@@ -1,78 +1,78 @@
 <template>
-  <v-layout id="CardsInfo" row justify-center>
-    <v-dialog
-      v-model="dialog"
-      fullscreen
-      hide-overlay
-      transition="dialog-bottom-transition"
-    >
-      <v-card elevation="4" class="text-xs-left pa-3">
-        <v-toolbar dark color="#6a1b9a">
-          <v-btn icon dark @click.native="dialog = false">
-            <v-icon>close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Tarjetas afiliadas</v-toolbar-title>
-          <v-spacer></v-spacer>
-        </v-toolbar>
-        <v-container fill-height>
-          <v-layout row wrap align-content-center justify-center>
-            <v-flex xs12 sm6 class="text-xs-center">
-              <v-card elevation="4" class="pa-3">
-                <v-list two-line>
-                  <template v-for="(item, index) in paymentInstruments">
-                    <v-divider :inset="true" :key="index + 'e'"></v-divider>
-                    <v-list-tile
-                      :key="item.id"
-                      avatar
-                      @click="handleSelectInstrument(index)"
-                    >
-                      <v-list-tile-avatar width="60px" class="avatarCard" tile>
-                        <v-img width="60px" :src="paymentInsturmentImg"></v-img>
-                      </v-list-tile-avatar>
-
-                      <v-list-tile-content>
-                        <v-list-tile-title
-                          v-html="item.payment_medium.name"
-                        ></v-list-tile-title>
-                        <v-list-tile-sub-title
-                          v-html="'**** **** **** ' + item.last4"
-                        ></v-list-tile-sub-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
-                  </template>
-                </v-list>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-divider></v-divider>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-list two-line>
+          <template v-for="(item, index) in paymentInstruments">
+            <v-divider :inset="true" :key="index + 'e'"></v-divider>
+            <v-list-tile
+              :key="item.id"
+              avatar
+              @click="handleSelectInstrument(item)"
+            >
+              <v-list-tile-avatar width="60px" class="avatarCard" tile>
+                <v-img width="60px" :src="paymentInsturmentImg"></v-img>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title
+                  v-html="item.payment_medium.name"
+                ></v-list-tile-title>
+                <v-list-tile-sub-title
+                  v-html="'**** **** **** ' + item.last4"
+                ></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
       </v-card>
-    </v-dialog>
+    </v-flex>
+    <v-btn
+      dark
+      fab
+      bottom
+      right
+      fixed
+      color="primary"
+      class="fab-btn"
+      @click="() => $router.push({ name: 'PaymentInstrumentForm' })"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
   </v-layout>
 </template>
+
 <script>
-import "../../../assets/utils";
+import { getPaymentInstrumentApi } from "@/api/modules";
 export default {
-  data() {
-    return {
-      dialog: false,
-      paymentInstruments: [],
-      paymentInsturmentImg: "static/paymentInstrument.png"
-    };
-  },
+  data: () => ({
+    paymentInstruments: [],
+    paymentInsturmentImg: "static/paymentInstrument.png"
+  }),
   methods: {
-    show(paymentInstruments) {
-      this.paymentInstruments = paymentInstruments;
-      this.dialog = true;
+    async getPaymentInstruments() {
+      var serviceResponse = await getPaymentInstrumentApi();
+      if (serviceResponse.ok) {
+        this.paymentInstruments = serviceResponse.data;
+      } else {
+        const params = { text: serviceResponse.message.text };
+        window.getApp.$emit("SHOW_ERROR", params);
+      }
     },
-    hide() {
-      this.dialog = false;
-    },
-    handleSelectInstrument(index) {
-      const paymentInstrument = this.paymentInstruments[index];
-      this.$emit("onSelectInstrument", paymentInstrument);
-      this.hide();
+    handleSelectInstrument(paymentInstrumentSelected) {
+      this.$emit("next", { paymentInstrumentSelected });
     }
+  },
+  async mounted() {
+    this.getPaymentInstruments();
   }
 };
 </script>
+<style>
+.avatarCard {
+  padding-right: 30px;
+}
+
+.v-stepper__wrapper {
+  height: 100%;
+}
+</style>
